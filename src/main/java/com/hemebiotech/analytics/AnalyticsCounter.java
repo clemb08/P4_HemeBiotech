18de6a9f4f0353e9614244676c47fb7aa95c90ad
clemb08 <clem.boschet@gmail.com>
2020-11-11 19:05:10 +0100

    Use of ReadSymptomDataFromFile Re-write the counter in the Main Class

--- src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,8 +1,13 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.analytics.reader.ReadSymptomDataFromFile;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -10,34 +15,31 @@ public class AnalyticsCounter {
 	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		// Read the file with the util class & return List of String
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptoms = reader.GetSymptoms();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		Map<String, Integer> existingSymptoms = new HashMap<String, Integer>();
+		int line = 0;
 
-			line = reader.readLine();	// get another symptom
+		for (String symptom : symptoms) {
+			line++;
+			//If the line is empty log the line number
+			if (symptom.equals(""))
+				System.out.println("line " + line + " is empty");
+			//If symptom exist increment the map's value
+			if (existingSymptoms.containsKey(symptom))
+				existingSymptoms.put(symptom, existingSymptoms.get(symptom) + 1);
+			else
+				//else put the symptom in the map and instantiate the value to one
+				existingSymptoms.put(symptom, 1);
+
+			// next generate output
+			FileWriter writer = new FileWriter("result.out");
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 }
